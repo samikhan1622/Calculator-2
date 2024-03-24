8d40efc0c8adb5c93704ba33104b943c81ba3ba1
Sami Sherzaman <153662652+samikhan1622@users.noreply.github.com>
2024-03-25 03:05:45 +0500

    Add files via upload
    This project is a calculator application built using TypeScript, designed specifically for handling personal data. The calculator allows users to input their personal bio data information to calculate their data, providing a convenient tool for monitoring and managing personal data.

---
+++ calculator2.ts
@@ -0,0 +1,42 @@
+import inquirer from 'inquirer';
+
+let calclator = await inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "what is your name?",
+    },
+    {
+        type: "input",
+        name: "age",
+        message: "what is your age?",
+    },
+    {
+        type: "input",
+        name: "gender",
+        message: "what is your gender?",
+    },
+    {
+        type: "input",
+        name: "work",
+        message: "what is your work?",
+    },
+    {
+        type: "input",
+        name: "city",
+        message: "what is your city?",
+    },
+    {
+        type: "input",
+        name: "phone",
+        message: "what is your phone number?",
+    },
+    {
+        type: "confirm",
+        name: "confirm",
+        message: "are you sure?",
+        default: true
+    },
+]);
+
+console.log(calclator);
